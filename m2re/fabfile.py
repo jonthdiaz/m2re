1b16430a13da2c2c56ed25ae401a3082d5b52b90
jonthdiaz <jonthdiaz@gmail.com>
2015-01-09 11:10:56 -0500

    uso de fabric

--- m2re/fabfile.py
+++ m2re/fabfile.py
@@ -3,26 +3,30 @@ import sys
 import os
 import time
 
-from fabric.api import settings, run, env
+from fabric.api import settings, run, env, local
 
 #Change this configuration
 from django.conf import settings
 sys.path.insert(0, os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))
 
 env.project_name = 'm2re'
-env.hosts = ['host1']
+#env.hosts = ['host1']
 # env.user = 'm2re'
 # env.password = 'm2re_2015'
 
-env.date = time.strftime('%Y%m%d%H%M%S')
-os.environ['DJANGO_SETTINGS_MODULE'] = '%(project_name)s.settings' % env
+# env.date = time.strftime('%Y%m%d%H%M%S')
+# os.environ['DJANGO_SETTINGS_MODULE'] = '%(project_name)s.settings' % env
 
-env.db_name = settings.DATABASES['default']['NAME']
-env.db_user = settings.DATABASES['default']['USER']
-env.db_password = settings.DATABASES['default']['PASSWORD']
-env.db_host = settings.DATABASES['default']['HOST']
-env.db_port = settings.DATABASES['default']['PORT']
+# env.db_name = settings.DATABASES['default']['NAME']
+# env.db_user = settings.DATABASES['default']['USER']
+# env.db_password = settings.DATABASES['default']['PASSWORD']
+# env.db_host = settings.DATABASES['default']['HOST']
+# env.db_port = settings.DATABASES['default']['PORT']
 
 
-def runserver(port='8010', ip='0.0.0.0'):
-    run('./manage.py runserver %s:%s --settings=%s.local_settings') % (ip, port, env.project_name)
+def runserver(port='8012', ip='0.0.0.0'):
+    local('./manage.py runserver %s:%s --settings=%s.local_settings' % (ip, port, env.project_name))
+
+
+def hello(name='world'):
+    print(u'hello {}'.format(name))
--- m2re/m2re/local_settings.py
+++ m2re/m2re/local_settings.py
@@ -2,7 +2,3 @@ from m2re.settings import *
 
 print 'Usando local_settings'
 DEBUG = True
-
-TEMPLATE_CONTEXT_PROCESSORS += (
-    'applications.context_processors.send_debug_to_template',
-)
