50f245b5828daa1a799ba327697df0a1c61a4152
jonthdiaz <jonthdiaz@gmail.com>
2015-01-23 07:48:59 -0500

    update config

--- m2re/settings.py
+++ m2re/settings.py
@@ -29,7 +29,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+if DEBUG:
+    DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
